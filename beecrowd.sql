7d30c6c5722ab188244b81ec77cf3615ab51ccb0
RafaelFrz <rafael.ferraz@unochapeco.edu.br>
2022-06-30 18:32:30 -0300

    views

--- beecrowd.sql
+++ beecrowd.sql
@@ -19,4 +19,91 @@ select
     id,
     name
 from products
-where price between 10 and 100
+where price < 10 or price > 100
+
+-- 2065 - Representantes Executivos
+
+select 
+    pdt.name,
+    pvd.name
+from products as pdt
+inner join providers as pvd on (pdt.id_providers = pvd.id)
+where pdt.id_categories = 6
+
+-- 2606 - Categorias
+
+select
+    pdt.id,
+    pdt.name
+from products as pdt
+inner join categories as ctg on (pdt.id_categories = ctg.id)
+where ctg.name like 'super%'
+
+-- 2607 - Cidades em Ordem Alfabética
+
+select
+    city
+from providers
+order by city asc
+
+-- 2608 - Maior e Menor Preço
+
+select
+    max(price),
+    min(price)
+from products
+
+-- 2609 - Produtos por Categorias
+
+select 
+    ctg.name,
+    sum(pdt.amount)
+from products as pdt
+inner join categories as ctg on (pdt.id_categories = ctg.id)
+group by ctg.id
+
+-- 2610 - Valor Médio dos Produtos
+
+select
+    round(
+        avg(price), 2
+    ) as price
+from products
+
+-- 2611 - Filmes de Ação
+
+select
+    mv.id,
+    mv.name
+from movies as mv
+inner join genres as gen on (mv.id_genres = gen.id)
+where gen.description like 'Action'
+
+-- 2613 - Filmes em Promoção
+
+-- 2614 - Locação em Setembro
+
+select
+    ctm.name,
+    rnt.rentals_date
+from rentals as rnt
+inner join customers as ctm on (rnt.id_customers = ctm.id)
+where rentals_date between '2016-09-01' and '2016-09-30'
+
+-- 2615 - Expandindo o Negocio
+
+select
+    city
+from customers
+group by city
+
+-- 2616 - Nenhuma Locação
+
+select
+    ctm.id,
+    ctm.name
+from customers as ctm
+where ctm.id not in (select id_customers from locations)
+
+-- 2617 - Fornecedor Ajax SA
+
